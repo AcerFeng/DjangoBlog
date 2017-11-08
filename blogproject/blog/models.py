2a23410756111b4c3421d7674b83e28a92868fe2
AcerFeng <qinlanfeng@foxmail.com>
2017-11-08 20:36:22 +0800

    提取 base.html；新增跳转详情页

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -46,6 +47,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
     # def __unicode__(self):
     #     return 
